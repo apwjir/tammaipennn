fa07c6eae03d491f9350f9bcae57549af5f59388
apwjir <110964387+apwjir@users.noreply.github.com>
2023-03-30 21:58:08 +0700

    Update average.cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main(int argc, char *argv[]){
+    if(argc > 1){
+    float sum = 0;
+    cout << "---------------------------------\n";
+    for(int i = 1; i < argc; i++){
+        sum += atoi(argv[i]);
+    }
+    cout << "Average of " << argc - 1 << " numbers = " << sum/(argc-1);
+    cout << "\n---------------------------------";
+    }
+    else cout << "Please input numbers to find average.";
+}
+
 
